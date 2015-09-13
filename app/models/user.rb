0f17c4a4f26b7bb43b326390cf2fd9def4b6f90d
Sarah Breiding <breiding.sarah@gmail.com>
2015-09-13 13:50:26 -0400

    remove user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable, :validatable, :trackable
   # :recoverable
-  devise :database_authenticatable, :registerable, :rememberable
+  devise :database_authenticatable, :rememberable
 end
